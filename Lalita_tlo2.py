38ec160f1b322879d60954d0e77911fcfeed208f
Lalita Chettri <harimayachettri49@gmail.com>
2023-04-18 15:49:00 +0530

    pushing the py file as well

---
+++ Lalita_tlo2.py
@@ -0,0 +1,9 @@
+length= float(input('Enter the length'))
+width = float(input('Enter the width'))
+area = length * width
+print('The area of rectangle is',area)
+pi=3.14
+radius=float(input('Enter the radius'))
+height=float(input('Enter the height'))
+volume = pi*(radius**2)*height
+print('The volume of cyclinder is',volume)
